d18e55fc3280ea68300bb8cf930c633519e39988
ie4fb <67971257+ie4fb@users.noreply.github.com>
2021-05-20 11:47:12 +0300

    initial structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import appStyles from './App.module.css';
+
+function App() {
+  return (
+    <div className={appStyles.page}>
+    </div>
+  );
+}
+
+export default App;
