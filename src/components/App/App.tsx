a65fc0147ad9d57c11be61b21d65216d97ee4cbb
Сарычев Александр Сергеевич <asarychev@mac102579.local>
2021-05-23 23:30:37 +0300

    add AppHeader

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import appStyles from './App.module.css';
+import React from "react";
+import appStyles from "./App.module.css";
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import AppHeader from "../AppHeader/AppHeader";
 
 function App() {
+  const [current, setCurrent] = React.useState("one");
+
   return (
     <div className={appStyles.page}>
+      <AppHeader />
+      <div style={{ display: "flex" }}>
+        <Tab value="one" active={current === "one"} onClick={setCurrent}>
+          One
+        </Tab>
+        <Tab value="two" active={current === "two"} onClick={setCurrent}>
+          Two
+        </Tab>
+        <Tab value="three" active={current === "three"} onClick={setCurrent}>
+          Three
+        </Tab>
+      </div>
     </div>
   );
 }
